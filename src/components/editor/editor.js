34516f388c7d3d24425570c9a36a98e77efc53ac
sidorovk <sidorovk@doxare.com>
2020-07-01 07:22:10 +0300

    Remove redundant component

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -34,28 +34,6 @@ const setUpCanvas  = ({gl}) => {
     gl.setSize(window.innerWidth, window.innerHeight);
 };
 
-function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef();
-
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(e) => setActive(!active)}
-            onPointerOver={(e) => setHover(true)}
-            onPointerOut={(e) => setHover(false)}>
-            <boxBufferGeometry attach="geometry" args={[10, 10, 10]} />
-            <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
-}
-
 function Loading() {
     return (
         <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
